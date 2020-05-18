e45f50d116122edf9ceabb50c94ab7e0dcb98608
Subhasish <subhasish469@gmail.com>
2020-05-18 11:28:16 +0530

    MainActivity  file committed

--- app/src/main/java/com/example/androidgit/MainActivity.java
+++ app/src/main/java/com/example/androidgit/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.i("myTag","message");
         Log.i("myTag","message");
+        Log.i("myTag","message");
     }
 }
